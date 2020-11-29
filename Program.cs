4b82776892b7c12bac907beb84e339a660a6653e
cwsheng <cwsheng1992@outlook.com>
2020-11-29 16:24:02 +0800

    修改程序运行检查

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace URLProtocolDemo
@@ -17,9 +20,20 @@ namespace URLProtocolDemo
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            var from = new Form1();
-            from.Args = args;
-            Application.Run(from);
+            Process instance = RunningInstance();
+            if (instance == null)
+            {
+                //1.1 没有实例在运行
+                var from = new Form1();
+                //显示输入参数
+                from.Args = args;
+                Application.Run(from);
+            }
+            else
+            {
+                //1.2 已经有一个实例在运行
+                HandleRunningInstance(instance);
+            }
         }
 
 
@@ -44,14 +58,14 @@ namespace URLProtocolDemo
                         register.SubKey = @"calldemo\DefaultIcon";
                         register.WriteRegeditKey("", $"{Application.ExecutablePath},1");
                     }
-
                     if (!register.IsSubKeyExist(@"calldemo\shell"))
                     {
                         register.CreateSubKey(@"calldemo\shell");
                         register.CreateSubKey(@"calldemo\shell\open");
                         register.CreateSubKey(@"calldemo\shell\open\command");
                         register.SubKey = @"calldemo\shell\open\command";
-                        register.WriteRegeditKey("", $"{Application.ExecutablePath} %1");
+                        //添加默认键
+                        register.WriteRegeditKey("", $"\"{Application.ExecutablePath}\" \"%1\"");
                     }
                 }
             }
@@ -61,5 +75,39 @@ namespace URLProtocolDemo
                 throw;
             }
         }
+
+
+        #region 确保程序只运行一个实例
+        private static Process RunningInstance()
+        {
+            Process current = Process.GetCurrentProcess();
+            Process[] processes = Process.GetProcessesByName(current.ProcessName);
+            //遍历与当前进程名称相同的进程列表 
+            foreach (Process process in processes)
+            {
+                //如果实例已经存在则忽略当前进程 
+                if (process.Id != current.Id)
+                {
+                    //保证要打开的进程同已经存在的进程来自同一文件路径
+                    if (Assembly.GetExecutingAssembly().Location.Replace("/", "\\") == current.MainModule.FileName)
+                    {
+                        //返回已经存在的进程
+                        return process;
+                    }
+                }
+            }
+            return null;
+        }
+        //3.已经有了就把它激活，并将其窗口放置最前端
+        private static void HandleRunningInstance(Process instance)
+        {
+            ShowWindowAsync(instance.MainWindowHandle, 1); //调用api函数，正常显示窗口
+            SetForegroundWindow(instance.MainWindowHandle); //将窗口放置最前端
+        }
+        [DllImport("User32.dll")]
+        private static extern bool ShowWindowAsync(System.IntPtr hWnd, int cmdShow);
+        [DllImport("User32.dll")]
+        private static extern bool SetForegroundWindow(System.IntPtr hWnd);
+        #endregion
     }
 }
